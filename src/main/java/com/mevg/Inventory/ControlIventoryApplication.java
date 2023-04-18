49b0b721ac37bebac79a4d0a61cf6ab81d7ed10f
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2023-04-18 13:07:08 -0600

    init

---
+++ src/main/java/com/mevg/Inventory/ControlIventoryApplication.java
@@ -0,0 +1,13 @@
+package com.mevg.Inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlIventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlIventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mevg/Inventory/ControlIventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mevg.Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlIventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
